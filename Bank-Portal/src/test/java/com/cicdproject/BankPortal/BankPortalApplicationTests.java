d6f2718f0575fa0fe7963dd74cc7d4077fab18f3
Cameron <Cameron.Coupland@cognizant.com>
2017-07-06 11:13:20 +1000

    Added Skeleton Code

---
+++ Bank-Portal/src/test/java/com/cicdproject/BankPortal/BankPortalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cicdproject.BankPortal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankPortalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
