b5c044e92f4d9fdc286a70dd1848676f44b3e20d
Cameron <Cameron.Coupland@cognizant.com>
2017-07-10 13:25:14 +1000

    configured a landing page, and initial end points for the login and signup pages

--- Bank-Portal/src/main/java/com/cicdproject/BankPortal/controller/HomePageController.java
+++ Bank-Portal/src/main/java/com/cicdproject/BankPortal/controller/HomePageController.java
@@ -1,16 +1,37 @@
 package com.cicdproject.BankPortal.controller;
 
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cicdproject.BankPortal.utities.HTMLUtils;
 
 @RestController
 @EnableAutoConfiguration
 public class HomePageController {
-
-	@RequestMapping(value = {"/"}, produces = MediaType.TEXT_PLAIN_VALUE)
-    public String accountPage() {
-		return "Hello World";
+	
+	@RequestMapping(value = {"/"}, produces = MediaType.TEXT_HTML_VALUE)
+    public String homePage() {
+		return HTMLUtils.getHtmlFromFile("src/main/resources/static/home.html");
+	}
+	
+	@RequestMapping(value = {"/login"},
+			params = {"loginEMail", "loginPass"},
+    		method = RequestMethod.POST,
+			produces = MediaType.TEXT_HTML_VALUE)
+    public String loginPage(@RequestParam("loginEMail") String loginEMail, @RequestParam("loginPass") String loginPass) {
+		return HTMLUtils.getHtmlFromFile("src/main/resources/static/under-construction.html");
+	}
+	
+	@RequestMapping(value = {"/signup"},
+			params = {"signupEMail", "signupPass"},
+    		method = RequestMethod.POST,
+			produces = MediaType.TEXT_HTML_VALUE)
+    public String signupPage(@RequestParam("signupEMail") String loginEMail, @RequestParam("loginPass") String loginPass) {
+		return HTMLUtils.getHtmlFromFile("src/main/resources/static/under-construction.html");
 	}
 }
